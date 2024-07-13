8f8f1b3c6e467a4267bd7ba4ef854bfe32a1aa9a
Surya_T <71425657+suryaT1@users.noreply.github.com>
2024-07-13 12:02:42 +0530

    Update ec2_locals.tf

--- local_values/ec2_locals.tf
+++ local_values/ec2_locals.tf
@@ -1,20 +1,20 @@
-provider "aws" {
+provider "aws" {  #provider block 
     region = "sa-east-1"
     access_key = var.acc
     secret_key = var.sec
 }
-locals {
+locals {    #locals block
   ami_ids = {
     sa-east-1 = "ami-123"
     eu-west-1 = "ami-456"
     na-south-1 = "ami-789"
   }
 }
-variable "region_name" {
+variable "region_name" {   #variables block
     default = "sa-east-1" 
 }
 
-variable "tags_details" {
+variable "tags_details" { #variables block
     type = map
     default  = {
         "project" = "development"
@@ -22,12 +22,20 @@ variable "tags_details" {
     }
 
 }
-locals {
+locals {   #locals block
   machine = ["first","second","third"]
 }
 
-resource "aws_instance" "multi_ec2" {
-    ami = lookup(local.ami_ids,var.region_name)
+resource "aws_instance" "multi_ec2" {  #resource block
+    ami = lookup(local.ami_ids,var.region_name) #using lookup function
+ /* lookup = lookup (map , ref value )
+in the above line 
+local.ami_ids = sa-east-1 = "ami-123"
+    eu-west-1 = "ami-456"
+    na-south-1 = "ami-789"
+var.region_name = sa-east-1
+lookup ({sa-east-1 = "ami-123" ,eu-west-1 = "ami-456" ,na-south-1 = "ami-789"}, sa-east-1) */
+
    instance_type = "t2.micro"
    #tags = {
    # Name = local.machine[count.index]
