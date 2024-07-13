7f0db89c84b89423a041638a9c07999e653abae7
Surya_T <71425657+suryaT1@users.noreply.github.com>
2024-07-13 10:12:53 +0530

    Create multi_regions.tf

---
+++ multi_regions.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = "sa-east-1"
+}
+provider "aws" {
+  region = "eu-west-1"
+  alias = "eu_west"
+}
+variable "ami_id" {
+  default = "ami-12345"  #sa-east-1
+}
+
+variable "ami_id_2" {
+  default = "ami-678901" #eu-west-1
+}
+resource "aws_instance" "new_vm" {
+  instance_type = "t2.micro" #by default it consider as sa-east-1
+  ami = var.ami_id
+  tags = {
+    Name = "new_vm"
+  }
+}
+
+resource "aws_instance" "new_vm_2" {
+  instance_type = "t2.micro"
+  ami = var.ami_id
+  provider = aws.eu_west    #defined the region
+  tags = {
+    Name = "new_vm_2"
+  }
+}
