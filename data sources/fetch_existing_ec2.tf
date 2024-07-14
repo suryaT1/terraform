eefe01c629a0cbf63306d44c02f70755c0b196b1
Surya_T <71425657+suryaT1@users.noreply.github.com>
2024-07-14 20:51:23 +0530

    Update fetch_existing_ec2.tf

--- data sources/fetch_existing_ec2.tf
+++ data sources/fetch_existing_ec2.tf
@@ -3,14 +3,14 @@ provider "aws" {
     secret_key = var.sec
     region = "sa-east-1"
 }
-data "aws_instance" "name1" {}  #trying to fecth ec2 data
+data "aws_instance" "name_1" {}  #trying to fecth ec2 data
 
  output "instance_name1" {      #printing the ec2 value
-  value = data.aws_instance.name11.id
+  value = data.aws_instance.name_1.id
  }
  
  resource "aws_instance" "new_machine" {  #creating new ec2 machine using existing ec2 machine details 
 
-  ami = data.aws_instance.name11.ami   #calling name1 ami ID
-  instance_type = data.aws_instance.name11.instance_type   #calling name1 instance type
+  ami = data.aws_instance.name_1.ami   #calling name1 ami ID
+  instance_type = data.aws_instance.name_1.instance_type   #calling name1 instance type
 }
