0273ed3a3ef4773c697f770b03aee83326a3620c
Surya_T <71425657+suryaT1@users.noreply.github.com>
2024-08-06 18:39:22 +0530

    Create Variables.tf

---
+++ multi_providers/Variables.tf
@@ -0,0 +1,19 @@
+variable "ami_sa_location" { #amazon_linux_ami
+ default = "ami-04eeb630f180cea95"
+}
+
+variable "ami_sa_location_ubuntu" { #amazon_ubuntu_ami
+ default = "ami-080111c1449900431"
+}
+
+variable "ami_west_location" {
+ default = "ami-08ca6be1dc85b0e84"
+}
+
+variable "machine_type" {
+   default = "t2.micro"
+}
+
+variable "ami_northeast" {
+ default = "ami-03350e4f182961c7f"
+}
